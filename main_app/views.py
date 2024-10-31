ab92d4321dfd9cdc62accec53835ed59f2362e81
Ayah Ahmed <ayahahmed729@gmail.com>
2024-10-31 09:16:05 +0300

    .

---
+++ main_app/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Add the Cat class & list and view function below the imports
+class Cat:  
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+cats = [
+  Cat('Lolo', 'tabby', 'foul little demon', 3),
+  Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+  Cat('Raven', 'black tripod', '3 legged cat', 4)
+]
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+def about(request):
+    return render(request, 'about.html')
+def cats_index(request):
+    return render(request, 'cats/index.html', { 'cats': cats })
